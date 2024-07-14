f700b59283d7fa180560d457f8c7feb357686998
Karthikeya Aluru <codingprofession@gmail.com>
2024-07-14 15:36:44 +0530

    function

--- script.js
+++ script.js
@@ -16,24 +16,23 @@ document.addEventListener('DOMContentLoaded', function() {
     moon.addEventListener('click', function() {
         body.classList.add('night');
     });
-});
 
+    const cursorDot = document.querySelector("[data-cursor-dot]");
+    const cursorOutline = document.querySelector("[data-cursor-outline]");
+    
+    window.addEventListener("mousemove", function(e) {
+        const posX = e.clientX;
+        const posY = e.clientY;
 
-const cursorDot = document.querySelector("[data-cursor-dot]");
-const cursorOutline = document.querySelector("[data-cursor-outline]");
-window.addEventListener("mousemove", function(e) {
+        cursorDot.style.left = `${posX}px`;
+        cursorDot.style.top = `${posY}px`;
 
-    const posX = e.clientX;
-    const posY = e.clientY;
+        cursorOutline.style.left = `${posX}px`;
+        cursorOutline.style.top = `${posY}px`;
 
-    cursorDot.style.left = `${posX}px`;
-    cursorDot.style.top = `${posY}px`;
-
-    cursorOutline.style.left = `${posX}px`;
-    cursorOutline.style.top = `${posY}px`;
-
-    cursorOutline.animate({
-        left: `${posX}px`,
-        top: `${posY}px`
-    }, {duration: 500, fill: "forwards"})
+        cursorOutline.animate({
+            left: `${posX}px`,
+            top: `${posY}px`
+        }, { duration: 500, fill: "forwards" });
+    });
 });
