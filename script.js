1d604b88e04aa6ae74b38f513d506e9d82117c14
Karthikeya Aluru <codingprofession@gmail.com>
2024-08-06 18:02:58 +0530

    new-pages

--- script.js
+++ script.js
@@ -172,3 +172,18 @@ function signUp() {
     });
   });
 /*Loader Animation*/
+
+
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 100) { 
+          $('#scroll').fadeIn(); 
+      } else { 
+          $('#scroll').fadeOut(); 
+      } 
+  }); 
+  $('#scroll').click(function(){ 
+      $("html, body").animate({ scrollTop: 0 }, 600); 
+      return false; 
+  }); 
+});
