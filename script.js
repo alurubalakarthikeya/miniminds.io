250383798ded491700ec97cfc3dcfd183c6f4f86
Karthikeya Aluru <codingprofession@gmail.com>
2024-08-07 18:21:11 +0530

    new-button

--- script.js
+++ script.js
@@ -174,17 +174,3 @@ function signUp() {
 /*Loader Animation*/
 
 
-$(document).ready(function(){ 
-  $(window).scroll(function(){ 
-      if ($(this).scrollTop() > 100) { 
-          $('#scroll').fadeIn(); 
-      } else { 
-          $('#scroll').fadeOut(); 
-      } 
-  }); 
-  $('#scroll').click(function(){ 
-      $("html, body").animate({ scrollTop: 0 }, 600); 
-      return false; 
-  }); 
-});
-
